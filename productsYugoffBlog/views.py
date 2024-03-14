6c351843620283dc2f284106bd5c390dc4d6ff0d
Yugoff <vv-yugov@yandex.ru>
2024-03-14 13:34:52 +0500

    Add templates portfolio

--- productsYugoffBlog/views.py
+++ productsYugoffBlog/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'productsYugoffBlog/index.html')
+    context = {
+        'title': 'Yugoff - Programmer, ML Engineer',
+        'portfolio': [
+            {
+                'image': 'static/site-yugoff-blog/image/logo-new.png',
+                'alt': 'Рус Групп ИЛАН',
+                'href': 'https://ilanbusiness.ru/',
+                'title': '«Рус Групп «ИЛАН»',
+                'types': 'ilanbusiness.ru'
+            },
+            {
+                'image': 'static/site-yugoff-blog/image/o-nas.png',
+                'alt': 'РСК ИЛАН',
+                'href': 'http://rsc-ilan.ru/',
+                'title': '«Ресурсоснабжающая компания «ИЛАН»',
+                'types': 'rsc-ilan.ru'
+            }
+        ]
+    }
+    return render(request, 'productsYugoffBlog/index.html', context)
